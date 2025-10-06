8afa56fa112dea7245c61f6eed47e0f68df38b00
niinikhvedelidze-lab <niinikhvedelidze@gmail.com>
2025-10-06 23:01:05 +0400

    Add files via upload

---
+++ GOA/level2/homework/hw2.py
@@ -0,0 +1,21 @@
+#არსებობს რამოდენიმე სახის მონაცემთა ტიპი, მათ შორის: int (integer), string, boolean, float, list(array), ა.შ.
+
+Int_variable = 10
+String_variable = "text"
+Bool1_variable = True
+Bool2_variable = False
+Float_variable = 5.5
+List_variable = [1, 2, 3, 4, 5]
+
+Int1_variable = 12
+Int1_variable = 15
+print(Int1_variable)
+
+number_that_is_stored_in_a_variable = 20 #ვიყენებთ ქვედა ტირეს რომ ცვლადის სახელი გახდეს მით უფრო მარტივად გასარჩევი/წასაკითხი.
+
+name = "Nino"
+print(name + 3) #ვერ შესრულდა მომხმარებლისგან მოცემული ბრძანება, რადგან ვლინდება ტიპიური შეცდობა. სტრინგთან რიცხვის შეჯამება მათემატიკურად შეუძლებელია.
+
+String1_variable = "Nino"
+String2_variable = "Khvedelidze"
+print(String1_variable, String2_variable)
