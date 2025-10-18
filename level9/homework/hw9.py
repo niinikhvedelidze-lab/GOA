0013beb591bd24d302364a8896e8e56f78ab51b6
niinikhvedelidze-lab <niinikhvedelidze@gmail.com>
2025-10-18 20:04:43 +0400

    hw9.py

--- level9/homework/hw9.py
+++ level9/homework/hw9.py
@@ -10,12 +10,13 @@ print(array_1)
 
 array_2 = [1, 2, 3, 8, 90, 101, 1090, 909, 10001, 1009, 1008]
 
-max_number = array_2[8]
+max_number = array_2[0]
 
 for num in array_2:
     if num > max_number:
-        num = max_number
-print("The biggest number:" , max_number)
+        max_number = num 
+
+print("The biggest number:", max_number)
 
 array_3 = [1, 2, 3, 4, 5]
 
@@ -44,4 +45,5 @@ array_6 = [1, 1, 3, 4, 5, 5, 7, 7, 8, 9, 10]
 array_6.sort()
 
 print(array_6)
+
 print(array_6.count(1))
